1ff688a778d9e738f4ce6facb433de2174014518
YanShengXuan <shengxuan.yan1995@gmail.com>
2024-11-18 09:03:07 +0800

    2024/11/15

--- src/tw/rocky/tutor/MySign.java
+++ src/tw/rocky/tutor/MySign.java
@@ -5,10 +5,8 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
 
-import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
 import javax.swing.JFileChooser;
